e9022b3f0b662dab076fbaa1d05c0ed87bb618d0
J Callan <jc1850@pc643s.cs.york.ac.uk>
2017-02-20 23:57:21 +0000

    changed headers

--- core/src/com/mygdx/game/GameTimer.java
+++ core/src/com/mygdx/game/GameTimer.java
@@ -3,14 +3,8 @@ package com.mygdx.game;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.utils.Timer;
 
-/**
- * @author Duck Related Team Name in Big Massive Letters
- * @since Assessment 2
- * @version Assessment 2
- *
- * An executable version of the game can be found at: https://jm179796.github.io/SEPR/DRTN-Assessment2.jar
- * Our website is: https://jm179796.github.io/SEPR/
- */
+
+
 public class GameTimer extends com.badlogic.gdx.scenes.scene2d.ui.Label {
 
     /**
